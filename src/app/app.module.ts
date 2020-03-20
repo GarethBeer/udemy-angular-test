7b94461fcd8f0dca07a80b90c527f5fcca1e2747
GarethBeer <gareth.beer1989@outlook.com>
2020-03-20 16:17:36 +0000

    2 way data binding used

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { ServerComponent } from "./server/server.component";
@@ -17,7 +18,7 @@ import { successAlertComponent } from "./SuccessAlert/successAlert.component";
     NavBarComponent,
     successAlertComponent
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,7 +6,20 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./servers.component.css"]
 })
 export class ServersComponent implements OnInit {
-  constructor() {}
+  allowNewServer = true;
+  serverCreationStatus = "No server was created";
+  serverName = "TestServer";
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = false;
+    }, 2000);
+  }
 
   ngOnInit(): void {}
+  onCreateServer = () => {
+    this.serverCreationStatus = "Server was created " + this.serverName;
+  };
+  onUpdateServerName = (event: Event) => {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  };
 }
